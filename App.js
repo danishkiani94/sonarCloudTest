873d2ddc89a903e3d9236417a11c0d659aa28bf1
DANISHKIANI <dkiani6@gmail.com>
2021-02-10 17:25:55 +0500

    Third test

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   let test =
   var
-  qeqweqwewqe ;
+  qeqweqwewqe 
+  qeqweqwewqe
   qeqweqwewqe
   return (
     <View style={styles.container}>
