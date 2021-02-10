9db62a25a7eeb76bbd8f45182a8f348ed70d8362
DANISHKIANI <dkiani6@gmail.com>
2021-02-10 16:36:06 +0500

    test

--- App.js
+++ App.js
@@ -3,9 +3,11 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  qeqweqwewqe
   return (
     <View style={styles.container}>
       <Text>Test application sonar</Text>
+      wqweqweqe
       <StatusBar style="auto" />
     </View>
   );
